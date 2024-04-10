d17f34b46a8e3b398e42df4e45409a62fdeceafd
Emmanuel Worgu <emmanuelworgu314@gmail.com>
2024-04-10 17:38:51 -0400

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import chalk from 'chalk';
+import cors from 'cors';
 import { config as configDotenv } from 'dotenv'; // Changed to named import 'config'
 
 import route from './Routes/index.js'; // Changed to import default export 'route'
@@ -31,6 +32,9 @@ configDotenv();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+
+app.use(cors());
 
 app.use(express.json());
 
